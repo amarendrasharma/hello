a43a003aa903bc027c87622a45ef80b9213e1054
Amarendra Sharma <s.amarendra1995@gmail.com>
2020-08-06 23:32:00 +0530

    tailwind form

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -9,7 +9,7 @@
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-first-name">
-                    First Name
+                    Your name please?
                 </label>
                 <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
@@ -18,7 +18,7 @@
             </div>
             <div class="w-full md:w-1/2 px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-last-name">
-                    Email
+                    Your email
                 </label>
                 <input
                     class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
@@ -28,14 +28,23 @@
         <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full px-3">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="grid-password">
-                    Password
+                    What you wanna tell me?
                 </label>
-                <input
-                    class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-                    id="grid-password" type="password" placeholder="******************">
+                <textarea class="resize border rounded focus:outline-none focus:shadow-outline" rows="5"
+                    cols="50"></textarea>
+
                 <p class="text-gray-600 text-xs italic">Make it as long and as crazy as you'd like</p>
             </div>
         </div>
+        <div class="flex flex-wrap -mx-3 mb-6">
+            <div class="w-full px-3">
+
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    Send Message
+                </button>
+            </div>
+        </div>
+
     </form>
 
 </div>
